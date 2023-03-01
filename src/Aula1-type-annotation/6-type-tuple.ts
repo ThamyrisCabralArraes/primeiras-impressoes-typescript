7b635f262826d49cd23b7e8028a006846936762d
thamyris <thamyriscabral@hotmail.com>
2023-03-01 09:36:37 -0300

    feat: adicionado exemplos de tuple

---
+++ src/Aula1-type-annotation/6-type-tuple.ts
@@ -0,0 +1,7 @@
+// tuple
+const dadosClientes1: readonly [string, number] = ['thamy', 1]
+const dadosClientes2: [string, ...number[]] = ['thamy', 1, 100]
+const dadosClientes3: [string, number, string?] = ['thamy', 1]
+
+// readonly array
+const array: ReadonlyArray<string> = ['a', 'b']
