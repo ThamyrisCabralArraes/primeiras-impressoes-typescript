03f43f831492797fd5588722acf8df423e4c61d4
thamyris <thamyriscabral@hotmail.com>
2023-03-01 19:26:25 -0300

    feat: adicionado exemplos de tipo assertions- coloquei quais sao os recomendados

--- src/Aula1-type-annotation/16-type-assertions.ts
+++ src/Aula1-type-annotation/16-type-assertions.ts
@@ -1,10 +1,14 @@
 const body1 = document.querySelector('body')
 if (body1) body1.style.background = 'red'
 
-// Non-null assertion
+// Non-null assertion (!) - nao recomendado
 const body2 = document.querySelector('body')!
 body2.style.background = 'red'
 
-//ty assertion
+//ty assertion - recomendado
 const body3 = document.querySelector('body') as HTMLBodyElement
 body3.style.background = 'red'
+
+
+// nao é recomendado
+const body4 = document.querySelector('body') as unknown as number
